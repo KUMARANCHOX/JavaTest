24e91c66da52d607c3ae5c8a531e52c7943fa5c0
Kumaran <baranikfs@gmail.com>
2020-07-15 14:03:21 +0530

    git trial commit

---
+++ JavaAltemetik/src/com/kumaran/AddController.java
@@ -0,0 +1,23 @@
+package com.kumaran;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddController {
+	@RequestMapping("/add")
+	public ModelAndView add(@RequestParam("first") int a, HttpServletRequest request,@RequestParam("second") int b,HttpServletResponse response) {
+		
+		int c= a+b;
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("result.jsp");
+		mv.addObject("result", c);
+		return mv;
+	}
+}
